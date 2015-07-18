974ec776686e27294ed7439885c45da3b05bdf4b
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-18 13:24:27 -0600

    logout route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "welcome#index"
   get "/auth/twitter", as: "/login", to: "sessions#new"
   get "/auth/twitter/callback", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
 end
