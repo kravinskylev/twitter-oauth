687b4db4d77aaa8fd3af47c180f96998087763da
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-18 13:21:10 -0600

    sessionc create login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,10 @@
 class SessionsController < ApplicationController
   def create
-    render text: request.env["omniauth.auth"].inspect
+    def create
+      if user = User.from_omniauth(request.env["omniauth.auth"])
+        session[:user_id] = user.id
+      end
+      redirect_to root_path
+    end
   end
 end
