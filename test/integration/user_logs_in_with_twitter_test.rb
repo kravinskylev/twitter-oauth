f6a922cd2243257a2cb07fba0f68569d6a4ec7c2
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-18 13:29:16 -0600

    add path assertions to test

--- test/integration/user_logs_in_with_twitter_test.rb
+++ test/integration/user_logs_in_with_twitter_test.rb
@@ -8,5 +8,9 @@ class UserLogsInWithTwitterTest < ActionDispatch::IntegrationTest
   test "logging in" do
     visit "/"
     assert_equal 200, page.status_code
+    click_link "login"
+    assert_equal "/", current_path
+    assert page.has_content?("Horace")
+    assert page.has_link?("logout")
   end
 end
